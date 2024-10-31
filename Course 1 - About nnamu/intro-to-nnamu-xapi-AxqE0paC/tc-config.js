32a70cdbbfc50c98ea54b13d313366dadaaa3fdc
alimullin <mary.mullin@nnamu.com>
2024-10-31 11:30:20 +0100

    Add files via upload

---
+++ Course 1 - About nnamu/intro-to-nnamu-xapi-AxqE0paC/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://iyuPg6o440sKCzGJ0TE3CaC-ZvB31tEd_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Intro to nnamu"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Hi newjoiner! We are so happy to have you on board and want to kick off your nnamu journey with an&amp;nbsp;intro course detailing who we are. We hope you enjoy learning a bit more about our history, mission, and future ambitions.&lt;/p&gt;&lt;p&gt;&lt;/p&gt;&lt;p&gt;You can find even&amp;nbsp;more details relating to the following lessons on the following confluence page: &lt;a rel=&#34;noopener noreferrer&#34; target=&#34;_blank&#34; href=&#34;https://nnamu.atlassian.net/wiki/x/AoCUCQ&#34;&gt;https://nnamu.atlassian.net/wiki/x/AoCUCQ&lt;/a&gt;.&lt;/p&gt;"
+};
