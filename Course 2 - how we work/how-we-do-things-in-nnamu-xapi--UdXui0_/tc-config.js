867ea03fbf44d12c4dd5d1c92f0a5ddc2d3fd3be
alimullin <mary.mullin@nnamu.com>
2024-10-31 11:32:42 +0100

    Add files via upload

---
+++ Course 2 - how we work/how-we-do-things-in-nnamu-xapi--UdXui0_/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://jljiAAwcy0OOZMTIhlcAq8o2OxCi9X7w_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "How we do things in nnamu!"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;&lt;span&gt;&lt;span style=&#34;color: rgb(0, 0, 0); text-decoration: inherit&#34;&gt;This course is designed to introduce new employees to the way we operate at nnamu, facilitating their smooth integration into the company.&lt;/span&gt;&lt;/span&gt;&lt;/p&gt;"
+};
